bb3d189f3de97ac21833d3d4f1d18c2f960cfa9d
nikhil6392 <nikhilpathak210431@gmail.com>
2025-03-06 18:55:56 +0530

    Navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './components/assets/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="flex items-center p-4 border-b bg-gray-700 text-white">
+      {/* Logo on the left */}
+      <img 
+        src={logo} 
+        alt="Logo" 
+        className="h-10 w-auto mt-[44px] ml-[100px]"
+      />
+
+      {/* Navigation links on the right */}
+      <ul className="flex space-x-6 ml-auto">
+        <li><a href="/" className="text-white">Home</a></li>
+        <li><a href="/services" className="text-white">Services</a></li>
+        <li><a href="/aboutUs" className="text-white">About us</a></li>
+        <li><a href="/blog" className="text-white">Blog</a></li>
+        <li><a href="/getEstimate" className="text-white ">Get estimate</a></li>
+      </ul>
+      <span className="ml-6 space-x-4">
+        <button className="px-4 py-2 bg-white text-black rounded-lg hover:bg-gray-200">
+          Borrower Portal
+        </button>
+        <button className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-blue-700">
+          Company Portal
+        </button>
+      </span>
+    </nav>
     </div>
   );
 }
 
 export default App;
+
